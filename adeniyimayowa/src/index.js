f6e79b2ad583587192ec340b9e0be54cf6353095
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-23 10:29:51 +0100

    index.js:

---
+++ adeniyimayowa/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const isGitHubPages = window.location.hostname.includes('github.io');
+const RouterToUse = isGitHubPages ? HashRouter : BrowserRouter;
+root.render(
+  // <React.StrictMode>
+  <RouterToUse>
+    <App />
+  </RouterToUse>
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
