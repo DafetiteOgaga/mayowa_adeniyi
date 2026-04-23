c0336f742022b5b3ea80ac453839c634e3ec136c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-23 10:32:12 +0100

    footer.js:

---
+++ adeniyimayowa/src/components/footer.js
@@ -0,0 +1,10 @@
+import { DafetiteFooter } from '../hooks/dafetiteFooter/dafetiteFooter'
+
+function Footer() {
+	return (
+		<footer className="text-center small mayor-footer">
+			<DafetiteFooter />
+		</footer>
+	)
+}
+export { Footer }
