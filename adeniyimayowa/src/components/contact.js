b39321f7004c5d880b37eaaaa389f7925e8a2362
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-23 10:32:09 +0100

    contact.js:

---
+++ adeniyimayowa/src/components/contact.js
@@ -0,0 +1,36 @@
+
+
+function Contact() {
+	return (
+		<section className="contact mayor-section-pad-top mayor-parallax-2 pb-0">
+			
+			<div className="container mayor-container-contact">
+				
+				<div className="row">
+					
+					<div className="text-center col-12">
+						<h2 className="mayor-section-title mb-4">Contact Me</h2>
+						<p className="mb-5">
+						Proin enim orci, tincidunt quis suscipit in, placerat nec est. Vestibulum posuere faucibus posuere. Quisque aliquam velit eget leo blandit egestas. Nulla id posuere felis, quis tristique nulla.
+						</p><br />
+					</div>
+					
+					<div className="col-sm-12 col-md-6">
+					<form action="" method="get">
+						<input id="name" name="name" type="text" placeholder="Your Name" className="mayor-input" required />
+						<input id="email" name="email" type="email" placeholder="Your Email" className="mayor-input" required />
+						<textarea id="message" name="message" rows="8" placeholder="Message" className="mayor-input" required></textarea>
+						<button type="submit" className="btn mayor-btn-submit">Submit</button>
+					</form>
+					</div>
+				</div>
+				{/* <!-- row ending --> */}
+				
+			</div>
+
+				{/* <Footer /> */}
+
+			</section>
+	)
+}
+export { Contact }
