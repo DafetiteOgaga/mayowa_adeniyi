25280fe57a368ca426bc353983714dcd491ea60a
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-23 10:32:08 +0100

    index.js:

---
+++ adeniyimayowa/src/components/index.js
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import { Header } from './header';
+import { Footer } from './footer';
+
+function Index() {
+	return (
+		<>
+			<Header />
+			<Outlet context={{}} />
+			<Footer />
+		</>
+	)
+}
+export { Index }
