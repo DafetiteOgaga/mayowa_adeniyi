93b97ac2877cc964c480dda1a21ca64942dc7621
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-27 01:25:54 +0100

    contact.js: removed commented codes

--- adeniyimayowa/src/components/contact.js
+++ adeniyimayowa/src/components/contact.js
@@ -3,18 +3,14 @@
 function Contact() {
 	return (
 		<>
-			
 			<div className="container mayor-container-contact">
-				
 				<div className="row">
-					
 					<div className="text-center col-12">
 						<h2 className="mayor-section-title mb-4">Contact Me</h2>
 						<p className="mb-5">
 						Proin enim orci, tincidunt quis suscipit in, placerat nec est. Vestibulum posuere faucibus posuere. Quisque aliquam velit eget leo blandit egestas. Nulla id posuere felis, quis tristique nulla.
 						</p>
 					</div>
-					
 					<div className="col-sm-12 col-md-6">
 					<form action="" method="get">
 						<input id="name" name="name" type="text" placeholder="Your Name" className="mayor-input" required />
@@ -24,13 +20,8 @@ function Contact() {
 					</form>
 					</div>
 				</div>
-				{/* <!-- row ending --> */}
-				
 			</div>
-
-				{/* <Footer /> */}
-
-			</>
+		</>
 	)
 }
 export { Contact }
