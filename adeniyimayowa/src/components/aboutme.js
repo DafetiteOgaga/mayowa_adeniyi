e788782104d188740c3bd6d5801915098b2a2d35
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-27 09:25:19 +0100

    aboutme.js: added animations with delays

--- adeniyimayowa/src/components/aboutme.js
+++ adeniyimayowa/src/components/aboutme.js
@@ -9,7 +9,7 @@ function AboutMe() {
                 <div className="row">
                     <div className="col-lg-12 col-xl-12">
                         <div className="mayor-intro-text-container">
-                            <h2 className="mayor-text-primary mb-4 mayor-section-title">Professional Experiences</h2>
+                            <h2 className="mayor-text-primary mb-4 mayor-section-title animate slide-from-top">Professional Experiences</h2>
                         </div>
                     </div>
                 </div>
@@ -20,11 +20,23 @@ function AboutMe() {
                     {/* <h3 className="mayor-text-primary underline mb-0">Experiences</h3> */}
                         {experiances.map((experience, eIdx) => {
                             return (
-                                <div key={eIdx} className="">
-                                    <div className="mayor-intro-text-container experience rm-pt">
-                                        <h3 className="mayor-text-primary mb-0">{experience.title}</h3>
-                                        <h5 className="mayor-text-primary mb-0 italic">{experience.company}</h5>
-                                        <p className="mb-4 mayor-intro-text">
+                                <div key={eIdx} className="animate slide-from-bottom-left">
+                                    <div className="mayor-intro-text-container experience rm-pt swap-animate swap-slide-right"
+                                    style={{
+                                        animationDelay: `${eIdx * 0.8}s`,
+                                    }}>
+                                        <h3 className="mayor-text-primary mb-0 swap-animate swap-slide-right"
+                                        style={{
+                                            animationDelay: `${eIdx * 0.8 * 1}s`,
+                                        }}>{experience.title}</h3>
+                                        <h5 className="mayor-text-primary mb-0 italic swap-animate swap-slide-right"
+                                        style={{
+                                            animationDelay: `${eIdx * 0.8 * 1.3}s`,
+                                        }}>{experience.company}</h5>
+                                        <p className="mb-4 mayor-intro-text swap-animate swap-slide-right"
+                                        style={{
+                                            animationDelay: `${eIdx * 0.8 * 1.6}s`,
+                                        }}>
                                             {experience.duties}
                                         </p>
                                     </div>
@@ -39,11 +51,23 @@ function AboutMe() {
                         <h3 className="certification-h3">Certifications</h3>
                             {certifications.map((cert, cIdx) => {
                                 return (
-                                    <div key={cIdx} className="">
-                                        <div className={`mayor-intro-text-container cert ${!cIdx?'rm-pt':''}`}>
-                                            <h5 className="mb-0">{cert.title}</h5>
-                                            <h6 className="mb-0 italic">{cert.cerification}</h6>
-                                            <p className="mb-4 mayor-intro-text">
+                                    <div key={cIdx} className="animate slide-from-bottom-right">
+                                        <div className={`mayor-intro-text-container cert ${!cIdx?'rm-pt':''} swap-animate swap-slide-left`}
+                                        style={{
+                                            animationDelay: `${cIdx * 0.8}s`,
+                                        }}>
+                                            <h5 className="mb-0 swap-animate swap-slide-left"
+                                            style={{
+                                                animationDelay: `${cIdx * 0.8 * 1}s`,
+                                            }}>{cert.title}</h5>
+                                            <h6 className="mb-0 italic swap-animate swap-slide-left"
+                                            style={{
+                                                animationDelay: `${cIdx * 0.8 * 1.3}s`,
+                                            }}>{cert.cerification}</h6>
+                                            <p className="mb-4 mayor-intro-text swap-animate swap-slide-left"
+                                            style={{
+                                                animationDelay: `${cIdx * 0.8 * 1.6}s`,
+                                            }}>
                                                 {cert.institution}
                                             </p>
                                         </div>
@@ -57,11 +81,23 @@ function AboutMe() {
                         <h3 className="education-h3">Education</h3>
                             {education.map((edu, eIdx) => {
                                 return (
-                                    <div key={eIdx} className="">
-                                        <div className={`mayor-intro-text-container edu ${!eIdx?'rm-pt':''}`}>
-                                            <h5 className="mb-0">{edu.course}</h5>
-                                            <h6 className="mb-0 italic">{edu.degree}</h6>
-                                            <p className="mb-4 mayor-intro-text">
+                                    <div key={eIdx} className="animate slide-from-bottom">
+                                        <div className={`mayor-intro-text-container edu ${!eIdx?'rm-pt':''} swap-animate swap-slide-bottom`}
+                                        style={{
+                                            animationDelay: `${eIdx * 0.8}s`,
+                                        }}>
+                                            <h5 className="mb-0 swap-animate swap-slide-bottom"
+                                            style={{
+                                                animationDelay: `${eIdx * 0.8 * 1}s`,
+                                            }}>{edu.course}</h5>
+                                            <h6 className="mb-0 italic swap-animate swap-slide-bottom"
+                                            style={{
+                                                animationDelay: `${eIdx * 0.8 * 1.3}s`,
+                                            }}>{edu.degree}</h6>
+                                            <p className="mb-4 mayor-intro-text swap-animate swap-slide-bottom"
+                                            style={{
+                                                animationDelay: `${eIdx * 0.8 * 1.6}s`,
+                                            }}>
                                                 {edu.school}
                                             </p>
                                         </div>
