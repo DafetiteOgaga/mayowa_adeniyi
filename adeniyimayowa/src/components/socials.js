875b43841b42e8d67bff3d18a7b69b27b3785b06
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-28 01:09:55 +0100

    socials.js: fixed the links and added linkedin, github and cog for dataanalyst

--- adeniyimayowa/src/components/socials.js
+++ adeniyimayowa/src/components/socials.js
@@ -1,27 +1,45 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from 'react-router-dom';
 import { useDevice } from "../context/deviceTypeContext";
+import { email, phoneNumber } from "../entry/entry";
 
 const commArr = [
 	{
-		icon: "comment",
-		name: "Chat Online",
-		// link: ""
+		icon: "linkedin",
+		name: "LinkedIn",
+		fab: "fab",
+		link: "https://www.linkedin.com/in/aden-emmanuel-117440142/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bb%2Fo04HcXRzabnbDCzJvVyQ%3D%3D"
 	},
+	{
+		icon: "github",
+		name: "GitHub",
+		fab: "fab",
+		link: "https://github.com/AdeniyiEmmanuel1?tab=overview&from=2026-02-01&to=2026-02-28"
+	},
+	{
+		icon: "chart-bar",
+		name: "Tableau",
+		link: "https://public.tableau.com/app/profile/adeniyi.emmanuel7932/vizzes"
+	},
+	// {
+	// 	icon: "comment",
+	// 	name: "Chat Online",
+	// 	// link: ""
+	// },
 	{
 		icon: "envelope",
 		name: "Send a Mail",
-		// link: "mailto:mail@company.com" // actual email address
+		link: `mailto:${email}`
 	},
-	{
-		icon: "map-marker-alt",
-		name: "Location",
-		// link: ""
-	},
+	// {
+	// 	icon: "map-marker-alt",
+	// 	name: "Location",
+	// 	// link: ""
+	// },
 	{
 		icon: "phone-square",
 		name: "Call",
-		// link: ""
+		link: `tel:${phoneNumber}`
 	}
 ]
 function Socials({setIsMenuOpen, isMenuOpen}) {
@@ -38,10 +56,10 @@ function Socials({setIsMenuOpen, isMenuOpen}) {
 					<div key={cIdx} className={`contact-item ${!cIdx?'ml-0':''}`}>
 						<Link
 						rel="nofollow"
-						// to=""
+						to={contact.link}
 						className={`item-link ${(isMobile&&isMenuOpen)?'overlay-icon-color':''}`}>
 							<FontAwesomeIcon
-							icon={contact.icon}
+							icon={contact.fab?[contact.fab, contact.icon]:contact.icon}
 							size={"lg"}
 							/>
 							{/* <span>&nbsp;</span> */}
