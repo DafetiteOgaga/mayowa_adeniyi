da2561eb7f0e192e2d405caab14305dedc8bae08
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-28 01:07:55 +0100

    aboutme.js: minor improvement

--- adeniyimayowa/src/components/aboutme.js
+++ adeniyimayowa/src/components/aboutme.js
@@ -1,5 +1,6 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { experiances, education, certifications } from "../entry/entry";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { experiences, education, certifications } from "../entry/entry";
+import { Link } from 'react-router-dom';
 
 function AboutMe() {
 	return (
@@ -18,7 +19,7 @@ function AboutMe() {
                     {/* experienes */}
                     <div className="">
                     {/* <h3 className="mayor-text-primary underline mb-0">Experiences</h3> */}
-                        {experiances.map((experience, eIdx) => {
+                        {experiences.map((experience, eIdx) => {
                             return (
                                 <div key={eIdx} className="animate slide-from-bottom-left">
                                     <div className="mayor-intro-text-container experience rm-pt swap-animate swap-slide-right"
@@ -29,7 +30,7 @@ function AboutMe() {
                                         style={{
                                             animationDelay: `${eIdx * 0.8 * 1}s`,
                                         }}>{experience.title}</h3>
-                                        <h5 className="mayor-text-primary mb-0 italic swap-animate swap-slide-right"
+                                        <h5 className="mb-0 italic swap-animate swap-slide-right"
                                         style={{
                                             animationDelay: `${eIdx * 0.8 * 1.3}s`,
                                         }}>{experience.company}</h5>
@@ -51,7 +52,8 @@ function AboutMe() {
                         <h3 className="certification-h3">Certifications</h3>
                             {certifications.map((cert, cIdx) => {
                                 return (
-                                    <div key={cIdx} className="animate slide-from-bottom-right">
+                                    <Link key={cIdx} to={cert.link}
+                                    className="cert-link animate slide-from-bottom-right">
                                         <div className={`mayor-intro-text-container cert ${!cIdx?'rm-pt':''} swap-animate swap-slide-left`}
                                         style={{
                                             animationDelay: `${cIdx * 0.8}s`,
@@ -71,7 +73,7 @@ function AboutMe() {
                                                 {cert.institution}
                                             </p>
                                         </div>
-                                    </div>
+                                    </Link>
                                 )
                             })}
                         </div>
